dee30aa8b3f038d4b1081ec7204f6fbfb7037489
DhanushaPathakota <55680546+DhanushaPathakota@users.noreply.github.com>
2024-03-19 11:33:40 -0700

    Add files via upload

---
+++ R/Analysis.R
@@ -0,0 +1,93 @@
+df <- read.csv(file = "Updated_Metrics.csv")
+
+
+df$status <- factor(df$status, levels = c(2,1), labels = c("G", "R"))
+
+df_no_I <- df[df$status %in% c("G", "R"),]
+
+
+df_no_na <- na.omit(df)
+df_no_I_na <- na.omit(df_no_I)
+
+
+
+IQR_Number_of_Forks <- IQR(df_no_I_na$Number_of_Forks, na.rm = TRUE)
+Q1_Number_of_Forks <- quantile(df_no_I_na$Number_of_Forks, 0.25, na.rm = TRUE)
+Q3_Number_of_Forks <- quantile(df_no_I_na$Number_of_Forks, 0.75, na.rm = TRUE)
+
+df_no_I_na_f <- df_no_I_na[df_no_I_na$Number_of_Forks > (Q1_Number_of_Forks - 1.5*IQR_Number_of_Forks) &
+                             df_no_I_na$Number_of_Forks < (Q3_Number_of_Forks + 1.5*IQR_Number_of_Forks), ]
+
+
+IQR_merge_to_fork_ratio <- IQR(df_no_I_na$merge_to_fork_ratio, na.rm = TRUE)
+Q1_merge_to_fork_ratio <- quantile(df_no_I_na$merge_to_fork_ratio, 0.25, na.rm = TRUE)
+Q3_merge_to_fork_ratio <- quantile(df_no_I_na$merge_to_fork_ratio, 0.75, na.rm = TRUE)
+
+df_no_I_na_m <- df_no_I_na[df_no_I_na$merge_to_fork_ratio > (Q1_merge_to_fork_ratio - 1.5*IQR_merge_to_fork_ratio) &
+                             df_no_I_na$merge_to_fork_ratio < (Q3_merge_to_fork_ratio + 1.5*IQR_merge_to_fork_ratio), ]
+
+IQR_Fork_Frequency <- IQR(df_no_I_na$Fork_Frequency, na.rm = TRUE)
+Q1_Fork_Frequency <- quantile(df_no_I_na$Fork_Frequency, 0.25, na.rm = TRUE)
+Q3_Fork_Frequency <- quantile(df_no_I_na$Fork_Frequency, 0.75, na.rm = TRUE)
+
+
+df_no_I_na_ff <- df_no_I_na[df_no_I_na$Fork_Frequency > (Q1_Fork_Frequency - 1.5*IQR_Fork_Frequency) &
+                              df_no_I_na$Fork_Frequency < (Q3_Fork_Frequency + 1.5*IQR_Fork_Frequency), ]
+
+IQR_Merge_Frequency <- IQR(df_no_I_na$Merge_Frequency, na.rm = TRUE)
+Q1_Merge_Frequency <- quantile(df_no_I_na$Merge_Frequency, 0.25, na.rm = TRUE)
+Q3_Merge_Frequency <- quantile(df_no_I_na$Merge_Frequency, 0.75, na.rm = TRUE)
+
+
+df_no_I_na_mf <- df_no_I_na[df_no_I_na$Merge_Frequency > (Q1_Merge_Frequency - 1.5*IQR_Merge_Frequency) &
+                              df_no_I_na$Merge_Frequency < (Q3_Merge_Frequency + 1.5*IQR_Merge_Frequency), ]
+
+
+
+model_forks <- glm(status ~ Number_of_Forks, data=df_no_I_na, family=binomial())
+summary(model_forks)
+
+#"Pseudo R-squared" and its p-value
+ll.null1 <- model_forks$null.deviance/-2
+ll.proposed1 <- model_forks$deviance/-2
+
+## McFadden's Pseudo R^2 = [ LL(Null) - LL(Proposed) ] / LL(Null)
+(ll.null1 - ll.proposed1) / ll.null1
+
+
+
+# For Merge to Fork Ratio
+model_mfratio <- glm(status ~ merge_to_fork_ratio, data=df_no_I_na_m, family=binomial())
+summary(model_mfratio)
+
+#"Pseudo R-squared" and its p-value
+ll.null2 <- model_mfratio$null.deviance/-2
+ll.proposed2 <- model_mfratio$deviance/-2
+
+## McFadden's Pseudo R^2 = [ LL(Null) - LL(Proposed) ] / LL(Null)
+(ll.null2 - ll.proposed2) / ll.null2
+
+
+# For Fork Frequency
+model_ffreq <- glm(status ~ Fork_Frequency, data=df_no_I_na_ff, family=binomial())
+summary(model_ffreq)
+
+#"Pseudo R-squared" and its p-value
+ll.null3 <- model_ffreq$null.deviance/-2
+ll.proposed3 <- model_ffreq$deviance/-2
+
+## McFadden's Pseudo R^2 = [ LL(Null) - LL(Proposed) ] / LL(Null)
+(ll.null3 - ll.proposed3) / ll.null3
+
+
+
+# For Merge Frequency
+model_mfreq <- glm(status ~ Merge_Frequency, data=df_no_I_na_mf, family=binomial())
+summary(model_mfreq)
+
+#"Pseudo R-squared" and its p-value
+ll.null4 <- model_mfreq$null.deviance/-2
+ll.proposed4 <- model_mfreq$deviance/-2
+
+## McFadden's Pseudo R^2 = [ LL(Null) - LL(Proposed) ] / LL(Null)
+(ll.null4 - ll.proposed4) / ll.null4
---
+++ R/Box_plots.R
@@ -0,0 +1,131 @@
+
+
+library(ggplot2)
+library(cowplot)
+library(ggstatsplot)
+library(mlbench)
+library(MASS)
+library(pROC)
+
+## Before removing outliers ##
+## Before removing outliers ##
+df <- read.csv(file = 'updated_metrics.csv')
+
+# Convert 'status' to a factor with "G" and "R" labels directly after loading the data
+df$status <- factor(df$status, levels = c(1, 2), labels = c("G", "R"))
+
+# Now, create df_no_I from the already converted df
+df_no_I <- df[df$status %in% c("G", "R"),]
+
+# Proceed with your data cleaning
+df_no_na <- na.omit(df)
+df_no_I_na <- na.omit(df_no_I)
+
+par(cex.lab=1)
+
+
+
+boxplot(Number_of_Forks ~ status, data = df_no_I_na, main = "Number_of_Forks", cex.main = 2, xlab = "Before removing Outliers" , at = c(1,2), col = c("light green","pink"))
+
+boxplot(merge_to_fork_ratio ~ status, data = df_no_I_na, main = "merge_to_fork_ratio", cex.main = 2, xlab = "Before removing Outliers", at = c(1,2), col = c("light green","pink"))
+boxplot(Merge_Frequency ~ status, data = df_no_I_na, main = "Merge_Frequency", cex.main = 2, xlab = "Before removing Outliers", at = c(1,2), col = c("light green","pink"))
+
+boxplot(Fork_Frequency ~ status, data = df_no_I_na, main = "Fork_Frequency", cex.main = 2, xlab = "Before removing Outliers", at = c(1,2), col = c("light green","pink"))
+
+
+
+
+
+## After removing outliers [Status all] ##
+
+Q <- quantile(df_no_na$Number_of_Forks, probs=c(.25, .75), na.rm = FALSE)
+iqr <- IQR(df$Number_of_Forks)
+up <-  Q[2]+1.5*iqr  
+low <- Q[1]-1.5*iqr
+freq_df <- subset(df_no_na, df_no_na$Number_of_Forks > (Q[1] - 1.5*iqr) & df_no_na$Number_of_Forks < (Q[2]+1.5*iqr))
+
+boxplot(Number_of_Forks ~ status, data = freq_df, main = "Number_of_Forks", cex.main = 2, xlab = "After removing Outliers" , at = c(1,2), col = c("light green","pink"))
+
+summary(freq_df$Number_of_Forks)
+
+Q <- quantile(df_no_na$merge_to_fork_ratio, probs=c(.25, .75), na.rm = FALSE)
+iqr <- IQR(df_no_na$merge_to_fork_ratio)
+up <-  Q[2]+1.5*iqr  
+low <- Q[1]-1.5*iqr
+dem_df <- subset(df_no_na, df_no_na$merge_to_fork_ratio > (Q[1] - 1.5*iqr) & df_no_na$merge_to_fork_ratio < (Q[2]+1.5*iqr))
+
+
+boxplot(merge_to_fork_ratio ~ status, data = dem_df, 
+        main = "Merge to Fork Ratio", cex.main = 2, 
+        xlab = "After removing Outliers", col = c("light green","pink"), ylim = c(0, 0.8))
+
+axis(side = 2, at = seq(0, 0.8, by = 0.2), labels = TRUE)
+
+summary(dem_df)
+
+
+
+Q <- quantile(df_no_na$Merge_Frequency, probs=c(.25, .75), na.rm = FALSE)
+iqr <- IQR(df_no_na$Merge_Frequency)
+up <-  Q[2]+1.5*iqr  
+low <- Q[1]-1.5*iqr
+cntr_df <- subset(df_no_na, df_no_na$Merge_Frequency > (Q[1] - 1.5*iqr) & df_no_na$Merge_Frequency< (Q[2]+1.5*iqr))
+
+boxplot(Merge_Frequency ~ status, data = cntr_df, main = "Merge_Frequency", cex.main = 2, xlab = "After removing Outliers", at = c(1,2), col = c("light green","pink"))
+
+
+Q <- quantile(df_no_na$Fork_Frequency, probs=c(.25, .75), na.rm = FALSE)
+iqr <- IQR(df_no_na$Fork_Frequency)
+up <-  Q[2]+1.5*iqr  
+low <- Q[1]-1.5*iqr
+add_df <- subset(df_no_na, df_no_na$Fork_Frequency > (Q[1] - 1.5*iqr) & df_no_na$Fork_Frequency < (Q[2]+1.5*iqr))
+
+boxplot(Fork_Frequency ~ status, data = add_df, main = "Fork_Frequency", cex.main = 2, xlab = "After removing Outliers", at = c(1,2), col = c("light green","pink"))
+
+#For communication data
+df <- read.csv(file = 'Communication_metrics.csv')
+
+
+
+par(cex.lab=1)
+
+IQR_count <- IQR(df_no_I_na$count, na.rm = TRUE)
+Q1_count <- quantile(df_no_I_na$count, 0.25, na.rm = TRUE)
+Q3_count <- quantile(df_no_I_na$count, 0.75, na.rm = TRUE)
+
+
+df_no_I_na_c <- df_no_I_na[df_no_I_na$count > (Q1_count - 1.5*IQR_count) &
+                             df_no_I_na$count < (Q3_count + 1.5*IQR_count), ]
+
+boxplot(count ~ status, data = df_no_I_na_c , main = "Number_of_emails_exchanged", cex.main = 2,at = c(1,2), col = c("light green","pink"))
+
+IQR_average_length <- IQR(df_no_I_na$average_length, na.rm = TRUE)
+Q1_average_length <- quantile(df_no_I_na$average_length, 0.25, na.rm = TRUE)
+Q3_average_length <- quantile(df_no_I_na$average_length, 0.75, na.rm = TRUE)
+
+
+df_no_I_na_a <- df_no_I_na[df_no_I_na$average_length > (Q1_average_length - 1.5*IQR_average_length) &
+                             df_no_I_na$average_length < (Q3_average_length + 1.5*IQR_average_length), ]
+
+boxplot( average_length~ status, data = df_no_I_na_a , main = "Average_length_of_messages", cex.main = 2,at = c(1,2), col = c("light green","pink"))
+
+IQR_number_active_senders <- IQR(df_no_I_na$number_active_senders, na.rm = TRUE)
+Q1_number_active_senders <- quantile(df_no_I_na$number_active_senders, 0.25, na.rm = TRUE)
+Q3_number_active_senders<- quantile(df_no_I_na$number_active_senders, 0.75, na.rm = TRUE)
+
+
+df_no_I_na_s <- df_no_I_na[df_no_I_na$number_active_senders > (Q1_number_active_senders - 1.5*IQR_number_active_senders) &
+                             df_no_I_na$number_active_senders < (Q3_number_active_senders + 1.5*IQR_number_active_senders), ]
+
+boxplot( number_active_senders~ status, data = df_no_I_na_s , main = "Number_of_Active_Senders", cex.main = 2,at = c(1,2), col = c("light green","pink"))
+
+
+IQR_frequency <- IQR(df_no_I_na$frequency, na.rm = TRUE)
+Q1_frequency <- quantile(df_no_I_na$frequency, 0.25, na.rm = TRUE)
+Q3_frequency<- quantile(df_no_I_na$frequency, 0.75, na.rm = TRUE)
+
+df_no_I_na_f <- df_no_I_na[df_no_I_na$frequency > (Q1_frequency- 1.5*IQR_frequency) &
+                             df_no_I_na$frequency < (Q3_frequency + 1.5*IQR_frequency), ]
+
+boxplot( frequency~ status, data = df_no_I_na_f , main = "Frequency_of_messaging", cex.main = 2,at = c(1,2), col = c("light green","pink"))
+
